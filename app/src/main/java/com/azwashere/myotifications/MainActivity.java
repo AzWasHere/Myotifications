c797b45809b119fa017bb0c1f7bc4c57f6f8e606
AzWasHere <az@not-jericho.com>
2015-10-23 18:07:03 -0500

    Added 3rd party app notifications

--- app/src/main/java/com/azwashere/myotifications/MainActivity.java
+++ app/src/main/java/com/azwashere/myotifications/MainActivity.java
@@ -4,13 +4,17 @@ import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.os.Bundle;
 import android.support.v4.content.LocalBroadcastManager;
 import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.ListView;
 import android.widget.Toast;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import eu.darken.myolib.Myo;
@@ -21,18 +25,33 @@ import eu.darken.myolib.msgs.MyoMsg;
 public class MainActivity extends AppCompatActivity {
     private static final String TAG = "Myotifications";
     private Toast mToast;
-    Context context;
     private Myo myo;
+    ListView list;
+    CustomListAdapter adapter;
+    ArrayList<Model> modelList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        startService(new Intent(this, NotificationService.class));
-        LocalBroadcastManager.getInstance(this).registerReceiver(onNotice, new IntentFilter("notify_rec"));
+        modelList = new ArrayList<Model>();
+        adapter = new CustomListAdapter(getApplicationContext(), modelList);
+        list = (ListView) findViewById(R.id.list);
+        list.setAdapter(adapter);
+        LocalBroadcastManager.getInstance(this).registerReceiver(onNotice, new IntentFilter("Msg"));
+        /*
+        TODO check and enable BT
+        TODO enable notifications check
+        */
 
-        MyoConnector connector = new MyoConnector(getContext());
+        MyoConnector connector = new MyoConnector(getApplicationContext());
         connector.scan(5000, new MyoConnector.ScannerCallback() {
+
+            /*
+            TODO Set connector to search for a Myo until found and connected
+            TODO Require sync, then unsync to activate
+            */
+
             @Override
             public void onScanFinished(List<Myo> myos) {
                 Myo myo = myos.get(0);
@@ -40,6 +59,10 @@ public class MainActivity extends AppCompatActivity {
                 myo.writeUnlock(MyoCmds.UnlockType.HOLD, new Myo.MyoCommandCallback() {
                     @Override
                     public void onCommandDone(Myo myo, MyoMsg msg) {
+                        /*
+                        TODO Look into sending a vibration command while device is asleep, or...
+                        TODO Turn EMG off and classifiers off, set connection speed to slow
+                        */
                         myo.writeVibrate(MyoCmds.VibrateType.LONG, null);
                     }
                 });
@@ -52,19 +75,72 @@ public class MainActivity extends AppCompatActivity {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.main, menu);
         return true;
+        /*
+        TODO create settings menu to enable or disable notifications for apps
+        */
     }
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        return super.onOptionsItemSelected(item);
+        switch (item.getItemId()) {
+            case R.id.action_settings:
+                Intent intent = new Intent(
+                        "android.settings.ACTION_NOTIFICATION_LISTENER_SETTINGS");
+                startActivity(intent);
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
     }
-
-
+    /*
+    TODO create disconnect button
+    TODO create wake up button
+    */
 
     private BroadcastReceiver onNotice = new BroadcastReceiver() {
+
+
         @Override
         public void onReceive(Context context, Intent intent) {
+            // String pack = intent.getStringExtra("package");
+            String title = intent.getStringExtra("title");
+            String text = intent.getStringExtra("text");
+            //int id = intent.getIntExtra("icon",0);
+
             myo.writeVibrate(MyoCmds.VibrateType.LONG, null);
+            Context remotePackageContext = null;
+            try {
+//                remotePackageContext = getApplicationContext().createPackageContext(pack, 0);
+//                Drawable icon = remotePackageContext.getResources().getDrawable(id);
+//                if(icon !=null) {
+//                    ((ImageView) findViewById(R.id.imageView)).setBackground(icon);
+//                }
+                byte[] byteArray = intent.getByteArrayExtra("icon");
+                Bitmap bmp = null;
+                if (byteArray != null) {
+                    bmp = BitmapFactory.decodeByteArray(byteArray, 0, byteArray.length);
+                }
+                Model model = new Model();
+                model.setName(title + " " + text);
+                model.setImage(bmp);
+
+
+                if (modelList != null) {
+                    modelList.add(model);
+                    adapter.notifyDataSetChanged();
+                } else {
+                    modelList = new ArrayList<Model>();
+                    modelList.add(model);
+                    adapter = new CustomListAdapter(getApplicationContext(), modelList);
+                    list = (ListView) findViewById(R.id.list);
+                    list.setAdapter(adapter);
+                }
+
+
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
         }
     };
 }
+
--- app/src/main/java/com/azwashere/myotifications/NotificationService.java
+++ app/src/main/java/com/azwashere/myotifications/NotificationService.java
@@ -1,14 +1,18 @@
 package com.azwashere.myotifications;
 
+import android.app.Notification;
 import android.content.Context;
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.os.Bundle;
 import android.service.notification.NotificationListenerService;
 import android.service.notification.StatusBarNotification;
+import android.util.Log;
 import android.support.v4.content.LocalBroadcastManager;
 
-/**
- * Created by Jeff on 10/9/2015.
- */
+import java.io.ByteArrayOutputStream;
+
+
 public class NotificationService extends NotificationListenerService {
 
     Context context;
@@ -21,14 +25,38 @@ public class NotificationService extends NotificationListenerService {
         context = getApplicationContext();
 
     }
-
     @Override
 
     public void onNotificationPosted(StatusBarNotification sbn) {
+        String pack = sbn.getPackageName();
+        String ticker ="";
+        if(sbn.getNotification().tickerText !=null) {
+            ticker = sbn.getNotification().tickerText.toString();
+        }
+        Bundle extras = sbn.getNotification().extras;
+        String title = extras.getString("android.title");
+        String text = extras.getCharSequence("android.text").toString();
+        int id1 = extras.getInt(Notification.EXTRA_SMALL_ICON);
+        Bitmap id = sbn.getNotification().largeIcon;
 
-        Intent notifyRec = new Intent("notify_rec");
 
-        LocalBroadcastManager.getInstance(context).sendBroadcast(notifyRec);
+        Log.i("Package",pack);
+        Log.i("Ticker",ticker);
+        Log.i("Title",title);
+        Log.i("Text",text);
+
+        Intent msgrcv = new Intent("Msg");
+        msgrcv.putExtra("package", pack);
+        msgrcv.putExtra("ticker", ticker);
+        msgrcv.putExtra("title", title);
+        msgrcv.putExtra("text", text);
+        if(id != null) {
+            ByteArrayOutputStream stream = new ByteArrayOutputStream();
+            id.compress(Bitmap.CompressFormat.PNG, 100, stream);
+            byte[] byteArray = stream.toByteArray();
+            msgrcv.putExtra("icon",byteArray);
+        }
+        LocalBroadcastManager.getInstance(context).sendBroadcast(msgrcv);
 
 
     }
@@ -36,6 +64,7 @@ public class NotificationService extends NotificationListenerService {
     @Override
 
     public void onNotificationRemoved(StatusBarNotification sbn) {
+        Log.i("Msg","Notification Removed");
 
     }
 }
